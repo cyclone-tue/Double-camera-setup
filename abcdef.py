b3bc83bdda81ea0694dd95979259ca6ef37e6768
Kobashimoto <joos.schoonwater@gmail.com>
2019-01-10 20:30:05 +0100

    Update abcdef.py

--- abcdef.py
+++ abcdef.py
@@ -9,7 +9,12 @@ def threshold(input,values):
     inputconv=cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
     output=cv2.inRange(inputconv, (low_h,low_s,low_v),(high_h, high_s, high_v))
     return output
-    
+
+def centreofmass(points):
+    return sum(points)/len(points)
+
+def av_dist_points_to_cm(cm,points):
+    return abs(cm-points)/len(points)
 
 while True:
     _, frame = cap.read()
@@ -18,7 +23,9 @@ while True:
     im2, contours, hierarchy = cv2.findContours(im,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     if (len(contours)>=5):
-        print(np.vstack(contours))
+        points=np.vstack(contours)
+        cm=centreofmass(points)
+        cv2.circle(frame,(int(cm[0][0]),int(cm[0][1])),5,(255,0,0),-1)
         fit=cv2.fitEllipse(np.vstack(contours))
         ellipse=cv2.ellipse(frame,fit,(255,0,0),5)
 
