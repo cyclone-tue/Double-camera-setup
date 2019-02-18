0240188765ef42dd3e645b40681e584105023f7c
robinferede <37348919+robinferede@users.noreply.github.com>
2019-02-18 15:07:59 +0100

    Changed ellipse fit to quadrelateral fit
    Fits a polygon
    if there are 4 corners the corners are passed to aruco.estimatePoseSingleMarkers

--- fitellipse.py
+++ fitellipse.py
@@ -1,9 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from cv2 import aruco
 import time
 
 cap = cv2.VideoCapture(0)
+cameraMatrix = np.array([
+    [6.3392788734453904e+02, 0., 3.0808675953434488e+02],
+    [0.,6.3040138666052906e+02, 2.0155498451453073e+02],
+    [0., 0., 1. ]
+    ])
+distCoeffs = np.array([ 1.7802315026160684e-01, -2.2294989847251276e+00,
+       -1.5298749543854606e-02, -3.3860921093235033e-04,
+       1.3272266506874113e+01])
 
 def threshold(image,values):
     low_h, low_s, low_v, high_h, high_s, high_v = values
@@ -17,20 +26,11 @@ def threshold(image,values):
     return output
 
 def weirdFilter(image):
-    kernel = np.ones((7,7),np.float32)
+    kernel = np.ones((10,10),np.float32)
     return cv2.filter2D(image,-1,kernel)
 
-def lowPass(image,cutoff):
-    shape = np.shape(image)
-    pad_shape = np.multiply(shape,(1-cutoff)/2).astype(int)
-    center = np.ones(np.multiply(shape,cutoff).astype(int),np.float32)
-    center = np.pad(center,((pad_shape[0],pad_shape[0]),(pad_shape[1],pad_shape[1])),'constant',constant_values=0)
-    magnitude_spectrum = cv2.normalize(center,0,255,cv2.NORM_MINMAX)
-    cv2.imshow("filter",magnitude_spectrum)
-    return np.multiply(image,np.pad(center,((0,np.shape(image)[0]-np.shape(center)[0]),(0,np.shape(image)[1]-np.shape(center)[1])),'constant',constant_values=0))
-    
 while True:
-    _,frame = cap.read()
+    frame = cv2.imread('WIN_20190211_16_22_23_Pro.jpg',1)
 
     imred =     threshold(frame,[160, 32, 160, 10,  150, 255]) #red filter
     imgreen =   threshold(frame,[ 70, 32, 160, 90, 150, 255]) #green filter
@@ -49,60 +49,23 @@ while True:
 
     combined = np.stack([imblue,imgreen,imred],axis=2)
     
-    _, contours, hierarchy = cv2.findContours(RGB,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(RGB,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
     if (len(contours) > 20):
         points = np.vstack(contours)
-        fit=cv2.fitEllipse(points)
-        ellipse=cv2.ellipse(frame,fit,(255,0,0),5)
+        outline = cv2.convexHull(points)
+        approx=cv2.approxPolyDP(outline,10,True)
+        
+        #if (np.shape(approx)[0] == 4):
+        #    rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(approx, 0.7, cameraMatrix, distCoeffs)
+        
+        cv2.drawContours(frame, [approx], 0, (255,0,0), 3)
 
-    cv2.imshow("rgb",combined)
-    cv2.imshow("filtered",weirdFilter(combined))
-    cv2.imshow("overlap",RGB)
     cv2.imshow("Frame+ellipse", frame)
-
-    """
-    #cv2.imshow("Red" , imred)
-    f = np.fft.fft2(imred)
-    fshift_red = np.fft.fftshift(f)
-    magnitude_spectrum = 20 * np.log(np.abs(fshift_red))
-    magnitude_spectrum = cv2.normalize(magnitude_spectrum,0,255,cv2.NORM_MINMAX)
-    #cv2.imshow("Red Fourier", magnitude_spectrum)
-
-    #cv2.imshow("Blue" , imblue)
-    f = np.fft.fft2(imblue)
-    fshift_blue = np.fft.fftshift(f)
-    magnitude_spectrum = 20 * np.log(np.abs(fshift_blue))
-    magnitude_spectrum = cv2.normalize(magnitude_spectrum,0,255,cv2.NORM_MINMAX)
-    #cv2.imshow("Blue Fourier", magnitude_spectrum)
-
-    cv2.imshow("Green" , imgreen)
-    f = np.fft.fft2(imgreen)
-    fshift_green = np.fft.fftshift(f)
-    #fshift_green = lowPass(fshift_green,1/2)
-    inverse = np.fft.ifftshift(fshift_green)
-    inverse = np.fft.ifft(inverse)
-    cv2.imshow("Green Filtered", cv2.normalize(np.abs(inverse), 0, 255, cv2.NORM_MINMAX))
-    magnitude_spectrum = 20 * np.log(np.abs(fshift_green)+1)
-    magnitude_spectrum = cv2.normalize(magnitude_spectrum,0,255,cv2.NORM_MINMAX)
-    cv2.imshow("Green Fourier", magnitude_spectrum)
-
-    #print(magnitude_spectrum)
-    #multiplied = fshift_green
-    multiplied = np.multiply(fshift_blue,fshift_green)
-    multiplied = np.multiply(multiplied,fshift_red)
-    multiplied = lowPass(multiplied,1/3)
-
-    #magnitude_spectrum = 20 * np.log(np.abs(multiplied)+1)
-    magnitude_spectrum = cv2.normalize(np.abs(multiplied),0,255,cv2.NORM_MINMAX)
-    #cv2.imshow("multiplied Fourier", magnitude_spectrum)
-
-    inverse = np.fft.ifftshift(multiplied)
-    inverse = np.fft.ifft2(inverse)
-    inverse = np.abs(inverse)
-    inverse = cv2.normalize(inverse,0,255,cv2.NORM_MINMAX)
-    #cv2.imshow("inverse", inverse)
-    """
+    cv2.imshow("overlap",RGB)
+    cv2.imshow("filtered",weirdFilter(combined))
+    cv2.imshow("rgb",combined)   
+    
  
     key = cv2.waitKey(1)
     if key == 27:
@@ -112,4 +75,5 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
 
