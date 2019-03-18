2ee6c643c6ffc358c9b63c9b83bbc45a24e36a5a
Kobashimoto <joos.schoonwater@gmail.com>
2019-03-18 15:15:07 +0100

    Update fitellipse.py

--- fitellipse.py
+++ fitellipse.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from numpy import linalg as LA
 
 cap = cv2.VideoCapture(0)
 
@@ -18,6 +19,18 @@ def weirdFilter(image):
     kernel = np.ones((10,10),np.float32)
     return cv2.filter2D(image,-1,kernel)
 
+def overlap(imred,imgreen,imblue):
+    A1 = cv2.bitwise_and(weirdFilter(imred), weirdFilter(imgreen))
+    A2 = cv2.bitwise_and(weirdFilter(imred), weirdFilter(imblue))
+    A3 = cv2.bitwise_and(weirdFilter(imgreen), weirdFilter(imblue))
+    B = cv2.bitwise_and(A1, imblue)
+    G = cv2.bitwise_and(A2, imgreen)
+    R = cv2.bitwise_and(A3, imred)
+
+    RGB = cv2.bitwise_or(R, G)
+    RGB = cv2.bitwise_or(RGB, B)
+    return RGB
+
 while True:
     _,frame = cap.read()
 
@@ -25,29 +38,41 @@ while True:
     imgreen =   threshold(frame,[70,32,160,100,150,255]) #green filter
     imblue =    threshold(frame,[110,32,160,140,150,255]) #blue filter
 
-    A1 = cv2.bitwise_and(weirdFilter(imred),weirdFilter(imgreen))
-    A2 = cv2.bitwise_and(weirdFilter(imred),weirdFilter(imblue))
-    A3 = cv2.bitwise_and(weirdFilter(imgreen),weirdFilter(imblue))
-    B = cv2.bitwise_and(A1,imblue)
-    G = cv2.bitwise_and(A2,imgreen)
-    R = cv2.bitwise_and(A3,imred)
-
-    RGB = cv2.bitwise_or(R,G)
-    RGB = cv2.bitwise_or(RGB,B)
+    RGB = overlap(imred,imgreen,imblue)
 
     combined = np.stack([imblue,imgreen,imred],axis=2)
     
-    _, contours, hierarchy = cv2.findContours(RGB,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(RGB,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
     if (len(contours) > 20):
         points = np.vstack(contours)
         fit=cv2.fitEllipse(points)
         ellipse=cv2.ellipse(frame,fit,(255,0,0),5)
+        (xc, yc), (a,b), theta = fit        # Aanpassen indien nodig
+
+        A = a**2*np.sin(theta)**2+b**2*np.cos(theta)**2
+        B = 2*(b**2-a**2)*np.sin(theta)*np.cos(theta)
+        C = a**2*np.cos(theta)**2+b**2*np.sin(theta)**2
+        D = -2*A*xc-2*B*yc
+        E = -B*xc-2*C*yc
+        F = A*xc**2+B*xc*yc+C*yc**2-a**2*b**2
+
+        # print([A,B,C,D,E,F])
+
+        Q=np.array([[A,B,D],[B,C,E],[D,E,F]])
+        w,v = LA.eig(Q)
+
+        print(w)
+        print(v)
+
+
 
     cv2.imshow("rgb",combined)
     cv2.imshow("filtered",weirdFilter(combined))
     cv2.imshow("overlap",RGB)
     cv2.imshow("Frame+ellipse", frame)
+
+
 
 
     key = cv2.waitKey(1)
