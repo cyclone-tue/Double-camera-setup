5e72681807d4ab5c968c810cb4a82b39fc8ab71a
RikHoek <49152647+RikHoek@users.noreply.github.com>
2019-05-16 11:26:32 +0200

    udated locatehoop2cams

--- locateHoopTwoCams.py
+++ locateHoopTwoCams.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-h = 1/600  # meters (on screen 1m away) per pixel (in camera picture)
-d = 0.1  # distance between cameras in meters
 
-
-def centercoor(cam1x, cam1y, cam2x, cam2y):
-    z = d/((cam1x-cam2x)*h)
-    x = z * cam1x * h
-    y = z * cam1y * h
+def centercoor(hoopx1, hoopy1, hoopx2, cam1, cam2):
+    d = cam2.pos[0]-cam1.pos[0]
+    h1 = 1/cam1.cameraMatrix[0][0]
+    h2 = 1/cam2.cameraMatrix[0][0]
+    z = d/(hoopx1 * h1 - hoopx2 * h2)
+    x = z * hoopx1 * h1
+    y = z * hoopy1 * h2
     coorditates = np.array([[x], [y], [z]])
     return coorditates
 
@@ -16,5 +16,3 @@ def orientation(a1,b1,a2,b2):
     exc1 = b1/a1
     exc2 = b2/a2
 
-
-print(centercoor(0.1,0,0.09,0))
